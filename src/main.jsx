01d1279bd9d2bd801217b801d3f42777dfd41c06
Hardy121 <merh0783@gmail.com>
2024-08-30 16:38:33 +0530

    doneeeee

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <div className='min-h-screen bg-[#0e0e0e] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px] text-white'>
     <App />
-  </StrictMode>,
+  </div>
 )
