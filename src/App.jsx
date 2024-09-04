46b65b91b336e262d2cde47a37e3d33225e59f54
Hardy121 <merh0783@gmail.com>
2024-09-04 10:36:52 +0530

    set up done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Service from "./pages/Service"
+import Portfolio from "./pages/Portfolio"
+import Contact from "./pages/Contact"
+
 function App() {
   return (
     <>
-      <div>
-        hello world
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <div className='min-h-screen bg-[#0e0e0e] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px] text-white'>
-    <App />
-  </div>
+  <BrowserRouter>
+    <div className='min-h-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]'>
+      <App />
+    </div>
+  </BrowserRouter>
 )
